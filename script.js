462e372e326a1d325d5dda0181be23e51fd41345
Nikhithareddy45 <nikkireddyperugu@gmail.com>
2024-10-30 13:16:57 +0530

    faq

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const login = document.querySelector('#login');
+    const signup = document.querySelector('#signup');
+    const getStarted = document.querySelector('#get-started');
+    const wedding = document.querySelector('#wedding');
+    const corporate = document.querySelector('#corporate');
+    const weddingBtn = document.querySelector('.wedding-btn');
+    
+    if (login) {
+        login.addEventListener("click", function() {
+            window.location.href = 'login.html';
+        });
+    }
+    
+    if (signup) {
+        signup.addEventListener("click", function() {
+            window.location.href = 'signup.html';
+        });
+    }
 
-const login = document.querySelector('#login');
-const signup = document.querySelector('#signup');
-const getStarted = document.querySelector('#get-started');
-const wedding = document.querySelector('#wedding');
-const corporate = document.querySelector('#corporate');
-const weddingBtn = document.querySelector('.wedding-btn');
-login.addEventListener("click",function(){
-    window.location.href = 'login.html'; 
-})
-signup.addEventListener("click",function(){
-    window.location.href = 'signup.html'; 
-})
-wedding.addEventListener("click",function(){
-    window.location.href = 'wedding.html'; 
-})
-corporate.addEventListener("click",function(){
-    window.location.href = 'corporate.html'; 
-})
-getStarted.addEventListener("click",function(){
-    window.location.href = 'contact.html'; 
-})
-weddingBtn.addEventListener("click",function(){
-    window.location.href = 'contact.html'; 
-})
+    if (wedding) {
+        wedding.addEventListener("click", function() {
+            window.location.href = 'wedding.html';
+        });
+    }
+
+    if (corporate) {
+        corporate.addEventListener("click", function() {
+            window.location.href = 'corporate.html';
+        });
+    }
+
+    if (getStarted) {
+        getStarted.addEventListener("click", function() {
+            window.location.href = 'contact.html';
+        });
+    }
+
+    if (weddingBtn) {
+        weddingBtn.addEventListener("click", function() {
+            window.location.href = 'contact.html';
+        });
+    }
+});
 
 document.addEventListener("DOMContentLoaded", function() {
     const menu = document.querySelector('.menu');
